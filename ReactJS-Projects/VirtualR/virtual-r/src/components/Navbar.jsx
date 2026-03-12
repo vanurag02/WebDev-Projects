70bf2346b01f232ad4085f3b0b4b2910433139bb
vanurag02 <anvaidya6@gmail.com>
2026-03-12 14:37:11 +0530

    VirtualR update | Mar 12, 2026

--- ReactJS-Projects/VirtualR/virtual-r/src/components/Navbar.jsx
+++ ReactJS-Projects/VirtualR/virtual-r/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
       <div className="container px-4 mx-auto relative text-sm">
         <div className="flex justify-between items-center">
-          <div className="flex items-center flex-shrink-0">
+          <div className="flex items-center shrink-0">
             <img src={logo} alt="" className="h-10 w-10 mr-2" />
             <span className="text-xl tracking-tight">VirtualR</span>
           </div>
@@ -26,12 +26,12 @@ const Navbar = () => {
             ))}
           </ul>
           <div className="hidden lg:flex justify-center space-x-12 items-center">
-            <a href="#" className="py-2 px-3 border rounded-md">
+            <a href="#" className="py-2 px-3 border rounded">
               Sign In
             </a>
             <a
               href="#"
-              className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+              className="bg-linear-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
             >
               Create an account
             </a>
@@ -42,6 +42,28 @@ const Navbar = () => {
             </button>
           </div>
         </div>
+        {mobileDrawerOpen && (
+          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+            <ul>
+              {navItems.map((item, index) => (
+                <li key={index} className="py-4">
+                  <a href={item.href}>{item.label}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-6">
+              <a href="#" className="py-2 px-3 border rounded">
+                Sign In
+              </a>
+              <a
+                href="#"
+                className="bg-linear-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+              >
+                Create an account
+              </a>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
